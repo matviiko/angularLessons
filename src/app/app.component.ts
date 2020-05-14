47cdff11c99124f2418d1c0f03e389d3b77ea2fa
matviiko <matviy.kopchak@gmail.com>
2020-05-14 19:46:12 +0300

    binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular-lessons';
+  // img = "https://w7.pngwing.com/pngs/791/922/png-transparent-react-javascript-library-redux-user-interface-tesseract-logo-symmetry-nodejs.png"
+  inputValue = ''
+
+  constructor() {
+    // setTimeout ( () => {
+    //   console.log('Timeout is over')
+    //   this.img = 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/cf/Angular_full_color_logo.svg/1200px-Angular_full_color_logo.svg.png'
+    // }, 5000)
+    // }
+  }
+
+  onInput(event: KeyboardEvent)
+  {
+    this.inputValue = (<HTMLInputElement>event.target).value
+  }
+
+  onBlur (str: string) {
+    this.inputValue = str
+  }
+
+  onClick () {
+    console.log("Click!")
+  }
+
 }
