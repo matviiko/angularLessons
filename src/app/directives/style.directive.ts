a952927758e9e58b38388320b90a716024f63ab1
matviiko <matviy.kopchak@gmail.com>
2020-05-16 23:15:18 +0300

    Send property in Directive

--- src/app/directives/style.directive.ts
+++ src/app/directives/style.directive.ts
@@ -1,14 +1,26 @@
-import {Directive, ElementRef, Renderer2} from '@angular/core';
+import {Directive, ElementRef, HostListener, Input, Renderer2} from '@angular/core';
 
 @Directive({
   selector: '[appStyle]'
 })
 export class StyleDirective {
+  @Input('appStyle') color: string = 'black'
 
   constructor(private el: ElementRef, private r: Renderer2 ) {
-    this.r.setStyle(this.el.nativeElement, 'color', 'blue');
     this.r.setStyle(this.el.nativeElement, 'font-size', '1rem');
     this.r.setStyle(this.el.nativeElement, 'text-align', 'center')
+  }
+
+@HostListener('click') onClick (event: Event) {
+    console.log(event)
+}
+
+@HostListener('mouseenter') onEnter () {
+  this.r.setStyle(this.el.nativeElement, 'color', this.color);
+}
+
+@HostListener('mouseleave') onLeave () {
+    this.r.setStyle(this.el.nativeElement, 'color', null);
   }
 
 }
