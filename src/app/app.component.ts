d6b990dd933c02ec61a8602ed9b8551afb1992f3
matviiko <matviy.kopchak@gmail.com>
2020-05-18 20:10:26 +0300

    Create Controls Form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormControl, FormGroup} from "@angular/forms";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+  form: FormGroup
 
+  ngOnInit() {
+    this.form = new FormGroup({
+      email: new FormControl(''),
+      password: new FormControl(null)
+    })
+  }
+
+  submit() {
+    console.log('Form submited: ', this.form)
+    let formData = {...this.form.value}
+    console.log('Form data: ', formData)
+  }
 }
