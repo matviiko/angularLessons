1e588fb4eae2682895765586d67371de3ce49f30
matviiko <matviy.kopchak@gmail.com>
2020-05-18 19:39:20 +0300

    Create Reactive Form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,10 @@
 import { Component } from '@angular/core';
-import {CounterService} from "./services/counter.service";
-import {LocalCounterService} from "./services/local-counter.service";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
-  providers: [LocalCounterService]
-
 })
 export class AppComponent {
-
-  constructor(public appCounterService: CounterService,
-              public localCounterService: LocalCounterService
-              ) {
-  }
-
-
 
 }
