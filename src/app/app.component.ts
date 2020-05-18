29e1382a463fdd09dab0a5bb2edcd97d01a32c6d
matviiko <matviy.kopchak@gmail.com>
2020-05-18 22:56:09 +0300

    made group from Form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,7 +12,11 @@ export class AppComponent implements OnInit{
   ngOnInit() {
     this.form = new FormGroup({
       email: new FormControl('', [Validators.email, Validators.required]),
-      password: new FormControl(null, [Validators.required, Validators.minLength(6)])
+      password: new FormControl(null, [Validators.required, Validators.minLength(6)]),
+      address: new FormGroup({
+        country: new FormControl('ua'),
+        city: new FormControl('', Validators.required)
+      })
     })
   }
 
