e8b662d05bbd28c68f3301e14b52bffb23c89cba
matviiko <matviy.kopchak@gmail.com>
2020-05-19 13:06:19 +0300

    add Group Form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,47 +1,59 @@
-import {Component, OnInit} from '@angular/core';
-import {FormArray, FormControl, FormGroup, Validators} from "@angular/forms";
+import {Component, OnInit} from '@angular/core'
+import {FormArray, FormControl, FormGroup, Validators} from '@angular/forms'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
+  styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit{
+export class AppComponent implements OnInit {
   form: FormGroup
 
   ngOnInit() {
     this.form = new FormGroup({
-      email: new FormControl('', [Validators.email, Validators.required]),
-      password: new FormControl(null, [Validators.required, Validators.minLength(6)]),
+      email: new FormControl('', [
+        Validators.email,
+        Validators.required
+      ]),
+      password: new FormControl(null, [
+        Validators.required,
+        Validators.minLength(6)
+      ]),
       address: new FormGroup({
-        country: new FormControl('ua'),
-        city: new FormControl('', Validators.required)
+        country: new FormControl('by'),
+        city: new FormControl('Минск', Validators.required)
       }),
       skills: new FormArray([])
     })
   }
 
   submit() {
-    console.log('Form submited: ', this.form)
-    let formData = {...this.form.value}
-    console.log('Form data: ', formData)
+    if (this.form.valid) {
+      console.log('Form: ', this.form)
+      const formData = {...this.form.value}
+
+      console.log('Form Data:', formData)
+      this.form.reset()
+    }
   }
 
   setCapital() {
     const cityMap = {
-      ru: 'Moscow',
-      ua: 'Kyiv',
-      by: 'Minsk'
+      ru: 'Москва',
+      ua: 'Киев',
+      by: 'Минск'
     }
+
     const cityKey = this.form.get('address').get('country').value
     const city = cityMap[cityKey]
-    this.form.patchValue({
-      address: {city: city}
-    })
+
+    this.form.patchValue({address: {city}})
   }
 
   addSkill() {
     const control = new FormControl('', Validators.required);
-    (this.form.get('skills') as FormArray).push(control)
+    (<FormArray>this.form.get('skills')).push(control)
+    // (this.form.get('skills') as FormArray).push(control)
   }
 }
+
